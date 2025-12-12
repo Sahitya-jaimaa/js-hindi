b33517263eecb12ff6659b678fda3c9eab206b5d
Sahitya <137879310+Sahitya-jaimaa@users.noreply.github.com>
2025-12-12 13:34:12 +0000

    variable

---
+++ 01basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId=1234
+let accountEmail="sahitya@gmail.com"
+var accountPassword="12345"
+accountCity="Jaipur"
+
+// accountId=4321
+
+accountEmail="raj@gmail.com"
+accountPassword="44332"
+accountCity="Bengaluru"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity])
